c66ff22bcad3b185754f411c9776258cce1321e3
Ilyas Oufakir <ouf.ilyas@gmail.com>
2022-12-08 11:02:21 -0500

    Add the possibility to hide controls for Plyr

--- script.js
+++ script.js
@@ -125,14 +125,14 @@ function init() {
     }
 
     // document.getElementById('videoLink').value = '';
-    let html = `<div class="plyr__video-embed" id="player">
+    let html = `<div class="plyr__video-embed" id="player" playsinline autoplay muted loop>
      <iframe src="${link}?origin=https://plyr.io&amp;iv_load_policy=3&amp;modestbranding=1&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;enablejsapi=1" allowfullscreen allowtransparency allow="autoplay"></iframe>
      </div>\n`;
 
     frame.innerHTML = html;
     settings.autoplay = autoplay.checked;
     if (controlBar.checked) {
-      settings.controls = [];
+      settings.controls = ['play'];
     } else {
       delete settings.controls;
     }
